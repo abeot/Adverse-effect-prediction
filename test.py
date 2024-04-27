83042cd4ff9e2f3c05eb7dc29a5b6b70c85bde01
abeot <66539445+abeot@users.noreply.github.com>
2024-04-27 15:37:08 -0600

    clean full dataset

--- test.py
+++ test.py
@@ -1,43 +1,46 @@
-from utils import plot_morgan, get_morgan, pairwise_similarity
+from utils import plot_morgan, get_morgan, pairwise_similarity, process
 import pandas as pd
 
 df = pd.read_csv(f'drug_ml_info_binary.csv')
+df = df[['drug', 'SMILES']]
 # plot_morgan(df, 'Tanimoto on entire dataset')
 
-sim_list = pairwise_similarity(get_morgan(df))
-name_list = df['drug'].tolist()
+df.to_csv('drug_structures.csv', index=False)
+
+# sim_list = pairwise_similarity(get_morgan(df))
+# name_list = df['drug'].tolist()
 
 
-num = len(name_list)
-assert num == len(sim_list)
+# num = len(name_list)
+# assert num == len(sim_list)
 
-matrix = []
+# matrix = []
 
-for i in range(num+1):
-    matrix.append([])
+# for i in range(num+1):
+#     matrix.append([])
 
-matrix[0].append(0)
-matrix[0] += name_list
+# matrix[0].append(0)
+# matrix[0] += name_list
 
-for i in range(1, num+1):
-    matrix[i].append(name_list[i-1])
-    for j in range(num):
-        matrix[i].append(sim_list[i-1][j])
+# for i in range(1, num+1):
+#     matrix[i].append(name_list[i-1])
+#     for j in range(num):
+#         matrix[i].append(sim_list[i-1][j])
 
-import csv
+# import csv
 
-with open('morgan.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(matrix)
+# with open('morgan.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(matrix)
 
-drug1 = 'ruxolitinib'
-drug2 = 'tofacitinib'
+# drug1 = 'ruxolitinib'
+# drug2 = 'tofacitinib'
 
-print(f'{matrix[129][592]}')
-for i in range(num+1):
-    if drug1 == matrix[i][0]:
-        for j in range(num+1):
-            if drug2 == matrix[0][j]:
-                print(f'{matrix[i][j]}')
-                break 
-        break
+# print(f'{matrix[129][592]}')
+# for i in range(num+1):
+#     if drug1 == matrix[i][0]:
+#         for j in range(num+1):
+#             if drug2 == matrix[0][j]:
+#                 print(f'{matrix[i][j]}')
+#                 break 
+#         break
