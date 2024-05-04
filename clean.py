1b1848682b25ae169cdddb0f3753200156d4709d
abeot <66539445+abeot@users.noreply.github.com>
2024-05-03 21:27:48 -0400

    change dataset and rerun model

--- clean.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('drug_ml_info_binary.csv')
-df = df.replace(-100, np.nan)
-df = df.dropna(axis=1, how='all')
-df = df.loc[:, (df != 0).any(axis=0)]
-
-df.to_csv('drug_ml_info_binary_1.csv', index=False)
